77eb2d149d4b9736acbbf96ce2c1b6a411697970
orucmu <mustafaoruc720@gmail.com>
2023-01-03 14:47:39 +0300

    Update Page created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const ejs = require('ejs');
 const Message = require('./models/Message')
 const app = express();
@@ -16,10 +17,11 @@ app.set("view engine", "ejs")
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'))
 
 //Routes
 app.get('/', async (req, res) => {
-    const messages = await Message.find({})
+    const messages = await Message.find({}).sort('-dateCreated')
     res.render('index', {
         messages
     })
@@ -44,6 +46,22 @@ app.post('/messages', async (req, res) => {
     await Message.create(req.body)
     res.redirect('/')
 });
+
+app.get('/post/edit/:id', async (req, res) => {
+    const post = await Message.findOne({ _id: req.params.id });
+    res.render('edit', {
+        post
+    })
+});
+
+app.put('/post/:id', async (req, res) => {
+    const post = await Message.findOne({ _id: req.params.id })
+    post.name = req.body.name
+    post.title = req.body.title;
+    post.message = req.body.message;
+    post.save();
+    res.redirect(`/post/${req.params.id}`)
+})
 
 const port = 3000;
 
